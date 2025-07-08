70200e89271d2eae4c949c766c7af5b9dbed092c
Knox Wong <wongknox@gmail.com>
2025-07-07 20:46:05 -0500

    Update schema.sql and .env files

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,20 @@
+DROP DATABASE IF EXISTS tasklist;
+CREATE DATABASE tasklist;
 
+\connect tasklist
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username text UNIQUE NOT NULL,
+    password text NOT NULL
+);
+
+CREATE TABLE tasks (
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    done boolean NOT NULL,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
